e96072fcfec3856aca3dd247a779544ff36653da
SeyyedAhmad RokniHosseini <seyyedahmadrokni@ut.ac.ir>
2023-11-19 23:03:02 +0330

    adder added

--- Adder.v
+++ Adder.v
@@ -4,19 +4,37 @@ module Adder();
 
     wire [7:0] nr_exp_little, nr_exp_big;
     wire [22:0] nr_mant_little, nr_mant_big;
-    reg [22:0] mant_diff;
+    reg [22:0] new_mant;
     wire [31:0] little, big;
-    Little_big lb(a, b, little, big);
+    reg sign;
 
-    Adder_normalizer adn(.exp_little(little[30:23]), .exp_big(big[30:23]), .mant_little(little[22:0]), .mant_b(big[22:0]), .exp_little_out(nr_exp_little), .exp_b_out(nr_exp_b), .mant_little_out(nr_mant_little), .mant_b_out(nr_mant_b));
+    Little_big lb(a, b, little, big);
+    Adder_normalizer adn(.exp_little(little[30:23]), .exp_big(big[30:23]), .mant_little(little[22:0]), .mant_big(big[22:0]), .exp_little_out(nr_exp_little), .exp_big_out(nr_exp_big), .mant_little_out(nr_mant_little), .mant_big_out(nr_mant_big));
+
+    wire [22:0] mant;
+    wire [7:0] exp;
+
     always @(a, b) begin
-        case ({a[31], b[31]})
-            2'b00: mant_diff = mant_a_out + mant_b_out;
-            2'b01: mant_diff = mant_a_out - mant_b_out;
-            2'b10: mant_diff = mant_b_out - mant_a_out;
-            2'b11: mant_diff = mant_a_out + mant_b_out;
+        case ({little[31], big[31]})
+            2'b00:begin
+                new_mant = nr_mant_little_out + nr_mant_big_out;
+                sign = 1'b0;
+            end
+            2'b01:begin
+                new_mant = nr_mant_big_out - nr_mant_little_out;
+                sign = 1'b1;
+            end
+            2'b10:begin
+                new_mant = nr_mant_big_out - nr_mant_little_out;
+                sign = 1'b0;
+            end
+            2'b11:begin
+                new_mant = nr_mant_little_out + nr_mant_big_out;
+                sign = 1'b1;
+            end
         endcase
+        out = {sign, exp, mant};
     end 
-    Shift_exp_mant sem()
+    Shift_exp_mant sem(nr_exp_little, new_mant, exp, mant);
 
 endmodule
--- Adder_normalizer.v
+++ Adder_normalizer.v
@@ -4,21 +4,13 @@ module Adder_normalizer(exp_little, exp_big, mant_little, mant_big, exp_little_o
     output [7:0] exp_little_out, exp_big_out;
     output [22:0] mant_little_out, mant_big_out;
 
-    reg [22:0] a_diff_big;
+    reg [22:0] little_diff_big;
     always @(*) begin
-        a_diff_big = exp_little - exp_big;
-        if (a_diff_big<23'b0)begin
-            a_diff_big = ~ a_diff_big;
-            exp_little_out = exp_big;
-            exp_big_out = exp_big;
-            mant_big_out = mant_big;
-            mant_little_out = mant_little >> a_diff_big;
-        end else begin
-            exp_little_out = exp_little;
-            exp_big_out = exp_little;
-            mant_little_out = mant_little;
-            mant_big_out = mant_big >> a_diff_big;
-        end
+        little_diff_big = exp_big - exp_little;
+        exp_little_out = exp_little;
+        exp_big_out = exp_little;
+        mant_little_out = mant_little;
+        mant_big_out = mant_big >> little_diff_big;
     end
 
 endmodule
