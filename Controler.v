3596c39f104e5f6fefd5e93a0ac4fc398391dc12
Ali <you@example.com>
2023-11-20 09:44:33 +0330

    bug controler fix

--- Controler.v
+++ Controler.v
@@ -1,7 +1,7 @@
 module  Controler(input start ,clk , valid , plu_done ,
      output reg start_plu ,we_a_reg ,eps_reg_we ,we_prim , rst , mux_sel , finish);
    
-   parameter [3:0] S=0, A=1,B=2,C=3,D=4,E=5,F=5,G=6,H=7;
+   parameter [3:0] S=0, A=1,B=2,C=3,D=4,E=5,F=6,G=7,H=8;
    reg [3:0] ps=S , ns;
 
 
--- OutputCheck.v
+++ OutputCheck.v
@@ -5,11 +5,14 @@ module OutputCheck(input clk , input [31 :0] x1 ,x2,x3,x4,a1,a2,a3,a4 , output v
     assign ch2 = ~ (| x2[30:0]);
     assign ch3 = ~ (| x3[30:0]);
     assign ch4 = ~ (| x4[30:0]);
+    reg xor_check , three_zero;
+    assign xor_check= ch1 ^ ch2 ^ ch3 ^ ch4;
+    assign three_zero =xor_check & ((ch1 & ch2) | (ch3 & ch4));
 
 
     MUX2 mx1(.sel(ch2) , .a(a2) , .b(a1) , .out(wire_mux1));
     MUX2 mx2(.sel(ch3) , .a(a3) , .b(wire_mux1) , .out(wire_mux2));
     MUX2 mx3(.sel(ch4) , .a(a4) , .b(wire_mux2) , .out(wire_mux3));
-    assign valid = ~(ch1 | ch2 | ch3 | ch4);
+    assign valid = (ch1 & ch2 & ch3 & ch4) | three_zero;
     Register reg3(.clk(clk) , .inp(wire_mux3) , .out(out) , .we(~valid));
 endmodule
