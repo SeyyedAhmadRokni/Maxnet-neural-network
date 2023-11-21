27f54fbcafc8e0c2ce78f611b9da5843303f8100
Ali <you@example.com>
2023-11-21 14:34:06 +0330

    ok TB

--- TestBench.v
+++ TestBench.v
@@ -4,24 +4,25 @@ module  TestBench();
     reg [31:0] mem [0:3] ;	
 	
 	initial begin
-		$readmemh ("instr.txt" , mem);
+		$readmemb ("./file/input.txt" , mem);
 	end
     
     reg [31:0] eps=32'b10111110010011001100110011001101 ,
-    a1 = 32'b01000110000111000011111110100111,
-    a2 = 32'b11000110000111000011111110100111,
-    a3 = 32'b00111111101001100110011001100110,
-    a4 = 32'b11000110000111000011111110100111;
+    a1,
+    a2,
+    a3,
+    a4;
     always #5 clk = ~clk;
     wire finish, overflow;
     wire [31 : 0] out;
     
     Maxnet_model mxn( start , clk ,eps , a1 , a2 , a3 , a4 ,  finish, overflow , out);
     initial begin
+        a1=mem[0] ; a2=mem[1];a3 = mem[2] ; a4=mem[3];
         #32 start=1;
         #10 start=0;
 
-        #2000;
+        #2000000;
         $stop;
     end
 
